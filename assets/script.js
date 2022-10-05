988ec319a92ff93250afc8d216f417c2b22921b7
theDomconrad <them.and.us.2013@gmail.com>
2022-10-05 00:51:48 -0700

    added html bootstrap time and color change function

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,43 @@
-// CURRENT DAY DISPLAYED ON TOP OF PLANNER
+let saveButton = $(".saveBtn");
+let signUpHour = $(".hour");
+let currentHour = $(moment().format("H"));
+
+
+function yolo() {
+     for (x = 9; x < 18; x++) {
+      if (x < 14) {
+        $("#" + x).addClass("past");
+      }
+      if (x == 14) {
+        $("#" + x).addClass("present");
+      }
+      if (x > 14) {
+        $("#" + x).addClass("future");
+      }
+     }
+  };
+yolo();
+
+
+
+ 
+
+   
+$("#currentDay").text(moment().format("MM DD YYYY, h a"));
+
+
 // TIMEBLOCKS LISTED
+
+
 // COLOR CODED PAST-PRESENT-FUTURE TIMEBLOCKS
+
+
 // CLICK TIMEBLOCK TO ENTER EVENT
+
+
 // EVENTS GET SAVED IN LOCAL STORAGE
+
+
 // SAVED EVENTS PERSIST WITH PAGE REFRESH
 
 //USE DATE LIBRARY FOR DATE AND TIME
-
