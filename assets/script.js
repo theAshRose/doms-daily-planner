4dd501bd27b0ace9b17cc0c2efb14e910184b0f2
theDomconrad <them.and.us.2013@gmail.com>
2022-10-05 19:20:45 -0700

    added notes for a higher grade

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
-let currentHour = moment().format("H");
-function yolo() {
-  for (x = 9; x < 18; x++) {
-    if (x < currentHour) {
+let currentHour = moment().format("H"); //keeping track of hour in military time
+function yolo() { //this function measures the current text block against current hour
+  for (x = 9; x < 18; x++) { //we use military time numbers, so all we have to do is
+    if (x < currentHour) {    //measure them against the hour with moment.js
       $("#" + x).addClass("past");
     }
     if (x == currentHour) {
@@ -13,19 +13,23 @@ function yolo() {
   }
 };
 yolo();
-$(".saveBtn").on("click", function () {
+$(".saveBtn").on("click", function() { //simply selecting all buttons through array, measuring them like we did above, then saving them to a key that matches their ID
   for (x = 9; x < 18; x++) {
     var agendaItem = [];
     agendaItem = $("#" + x).val();
     localStorage.setItem("#" + x, agendaItem);
   }
 });
-let populatePlanner = function () {
+let populatePlanner = function () { //this pulls the data by key and populates the info to the # matching the key :)
   for (x = 9; x < 18; x++) {
     let frisbee = localStorage.getItem("#" + x) || [];
     let target = $("#" + x)
     $(target).text(frisbee);
   }
 }
-populatePlanner();
-$("#currentDay").text(moment().format("MMMM Do YYYY"));
+populatePlanner(); //this makes sure our panner is up to date, the function above
+$("#currentDay").text(moment().format("MMMM Do YYYY")); //displays current day
+
+// console.log(($(event.target).parent().siblings(1).val('' + x)));
+// if (($(this).siblings().eq(2).attr("id")) == $("#" + x)) {
+  
